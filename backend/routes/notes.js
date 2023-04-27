6e39ceec3ac5be95f44dc7074699fca3442f9879
Tonmoy Sardar <tonmay10fcb@gmail.com>
2023-04-27 15:52:34 +0530

    add note endpoint repaired

--- backend/routes/notes.js
+++ backend/routes/notes.js
@@ -1,47 +1,47 @@
 // installed express
-const express=require('express');
+const express = require('express');
 // installed model schemas
 const Notes = require('../models/Notes');
 // installed middleware
 const fetchuser = require('../middleware/fetchuser');
 // installed router
-const router=express.Router();
+const router = express.Router();
 // installed express validation for data validation
 const { body, matchedData, validationResult } = require('express-validator');
 
 
 // GETNOTES ROUTE to fetch all notes and display them
-router.get('/getnotes',fetchuser,async(req,res)=>{
-   
+router.get('/getnotes', fetchuser, async (req, res) => {
+
     try {
-         // finding user notes by "fetchuser" middleware to find user and its notes
-    const notes= await Notes.find({user:req.user.id})
-    // sending all available notes as respond
-    res.json({notes,user:req.user.id})
+        // finding user notes by "fetchuser" middleware to find user and its notes
+        const notes = await Notes.find({ user: req.user.id })
+        // sending all available notes as respond
+        res.json({ notes, user: req.user.id })
     } catch (error) {
         res.status(500).send('Internal Server Error');
     }
 })
 
 // ADDNOTE ROUTE to create a note and save it in database
-router.post('/addnote',[
+router.post('/addnote', fetchuser, [
     body('title', "Enter Valid Title").isLength({ min: 3 }),
     body('description', "Enter Valid Description").isLength({ min: 10 }),
     body('tag', "Enter Valid Tag").isLength({ min: 2 })
-],async(req,res)=>{
+], async (req, res) => {
     // showing error for invalid inputs
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
         res.status(400).json({ errors: errors.array() });
     }
     try {
-        const {title,description,tag}=req.body;
-        const note=new Notes({
-            user: req.user.id, title, description, tag
+        const { title, description, tag } = req.body;
+        const note = new Notes({
+            title, description, tag, user: req.user.id
         })
         const savednote = await note.save();
         res.json(savednote)
-        
+
     } catch (error) {
         console.log(error)
         res.status(500).send('Internal Server Error');
@@ -54,4 +54,4 @@ router.post('/addnote',[
 
 
 
-module.exports=router;
+module.exports = router;
