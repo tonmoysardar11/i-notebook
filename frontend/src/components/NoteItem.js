9cca957199a46133eed636393ed05aaba4317eca
Tonmoy Sardar <tonmay10fcb@gmail.com>
2023-05-19 17:07:24 +0530

    edit note modal opening on frontend

--- frontend/src/components/NoteItem.js
+++ frontend/src/components/NoteItem.js
@@ -14,35 +14,7 @@ const NoteItem = (props) => {
     // getting props of notes and displaying in component in cards
     return (
         <>
-            {/* <button type="button" ref={ref} className="btn btn-primary d-none" data-bs-toggle="modal" data-bs-target="#exampleModal">
-                Launch demo modal
-            </button>
-
-            <div className="modal fade" id="exampleModal" tabIndex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-                <div className="modal-dialog">
-                    <div className="modal-content">
-                        <div className="cardModal">
-                            <span className="title">Edit Note</span>
-                            <form className="form">
-                                <div className="group">
-                                <input type="text" id="etitle" name='etitle' value={note.title} onChange={change} />
-                                    <label htmlFor="etitle">Title</label>
-                                </div>
-                                <div className="group">
-                                <input type="text" id="etag" name='etag' value={note.tag} onChange={change} />
-                                    <label htmlFor="etag">Tag</label>
-                                </div>
-                                <div className="group">
-                                <textarea type="text" rows='4' id="edesc" name="edesc" value={note.desc} onChange={change} />
-                                <label htmlFor="edesc">Desciption</label>
-                                </div>
-                                <button type="submit" onClick={onSubmit}>Submit</button>
-                            </form>
-                        </div>
-                    </div>
-                </div>
-            </div> */}
-
+            
 
             <div className='col-lg-4 col-md-6 col-sm-12'>
                 <div className="card mx-2 my-2" >
@@ -51,7 +23,7 @@ const NoteItem = (props) => {
                     <br />
                     <p className='mx-4 my-2'>Tag: {props.data.tag}</p>
                     <div id="btnGrp">
-                        <button className='uiBtn my-2 mx-2 edit'> <i className="fa-solid fa-pen-to-square" ></i></button>
+                        <button className='uiBtn my-2 mx-2 edit'> <i className="fa-solid fa-pen-to-square" onClick={()=>props.updateNote(props.data)}></i></button>
                         <button className='uiBtn my-2 delete'> <i className="fa-solid fa-trash" onClick={(x) => deleteNotes(props.data._id)}></i></button>
                     </div>
                 </div>
--- frontend/src/components/NoteSection.js
+++ frontend/src/components/NoteSection.js
@@ -1,5 +1,5 @@
 
-import React, { useContext, useEffect } from 'react'
+import React, { useState, useContext, useEffect, useRef } from 'react'
 import noteContext from '../context/notes/noteContext';
 import NoteItem from './NoteItem';
 
@@ -13,16 +13,74 @@ const NoteSection = () => {
         getNotes();
         // eslint-disable-next-line
     }, []);
+
+    const [note, setNote] = useState({ etitle: '', edescription: '', etag: '' });
+
+    const ref = useRef(null);
+
+    const updateNote = (currentNote) => {
+        ref.current.click();
+        setNote({ ...note, etitle: currentNote.title, edescription: currentNote.description, etag: currentNote.tag });
+
+    }
+
+    const change = (e) => {
+        setNote({ ...note, [e.target.name]: e.target.value })
+    }
+
+    const onSubmit = (e) => {
+        e.preventDefault();
+
+    }
+
+
+
+
     return (
-        // iterating the data by using array map method with unique key of data id
-        <div className=" container row my-2">
-            <h2>Your Notes</h2>
-            {data.map((x) => {
-                return <NoteItem key={x._id} data={x} />
-            })}
+        <>
+            {/* modal */}
+            <button type="button" ref={ref} className="btn btn-primary d-none" data-bs-toggle="modal" data-bs-target="#exampleModal">
+                Launch demo modal
+            </button>
+
+            <div className="modal fade" id="exampleModal" tabIndex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                <div className="modal-dialog">
+                    <div className="modal-content">
+                        <div className="cardModal">
+                            <span className="title">Edit Note</span>
+                            <form className="form">
+                                <div className="group">
+                                    <input type="text" id="etitle" name='etitle' value={note.etitle} onChange={change} />
+                                    <label htmlFor="etitle">Title</label>
+                                </div>
+                                <div className="group">
+                                    <input type="text" id="etag" name='etag' value={note.etag} onChange={change} />
+                                    <label htmlFor="etag">Tag</label>
+                                </div>
+                                <div className="group">
+                                    <textarea type="text" rows='4' id="edescription" name="edescription" value={note.edescription} onChange={change} />
+                                    <label htmlFor="edesc">Desciption</label>
+                                </div>
+                                <button className="logo2" type="submit" onClick={onSubmit}>Submit</button>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
 
 
-        </div>
+
+
+            {/*iterating the data by using array map method with unique key of data id*/}
+            <div className=" container row my-2">
+                <h2>Your Notes</h2>
+                {data.map((x) => {
+                    return <NoteItem key={x._id} updateNote={updateNote} data={x} />
+                })}
+
+
+            </div>
+        </>
     )
 }
 
